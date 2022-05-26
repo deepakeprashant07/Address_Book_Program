5be0e731d76dde5e1b7a9989bb3217db61c34222
Prashant Deepake <deepakeprashant1@gmail.com>
2022-05-27 00:44:06 +0530

    ADDED : EDIT PERSON INFORMATION

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,20 +4,27 @@ public class AddressBook {
     public static void main(String[] args) {
         System.out.println(":: WELCOME TO ADDRESS BOOK PROGRAM ::");
         PersonInfo person = new PersonInfo();
+        String name;
         Scanner scan = new Scanner(System.in);
         while (true) {
             System.out.println("1] ADD PERSON INFORMATION");
-            System.out.println("2] HISTORY");
-            System.out.println("3] Exit");
+            System.out.println("2] EDIT PERSON INFORMATION");
+            System.out.println("3] HISTORY");
+            System.out.println("4] Exit");
             int choice = scan.nextInt();
             switch (choice) {
                 case 1:
                     person.addPerson();
                     break;
                 case 2:
-                    person.display();
+                    System.out.println("ENTER PERSON NAME");
+                    name = scan.next();
+                    person.editInformation(name);
                     break;
                 case 3:
+                    person.display();
+                    break;
+                case 4:
                     System.exit(0);
             }
         }
--- src/PersonInfo.java
+++ src/PersonInfo.java
@@ -5,10 +5,10 @@ public class PersonInfo {
     public String firstName, lastName, address, city, state;
     public String zip, phoneNumber, emailId;
     ArrayList<PersonInfo> person = new ArrayList<PersonInfo>();
+    Scanner scan = new Scanner(System.in);
 
     public void addPerson() {
         PersonInfo p = new PersonInfo();
-        Scanner scan = new Scanner(System.in);
         System.out.println("ENTER PERSON FIRST NAME AND LAST NAME");
         p.firstName = scan.next();
         p.lastName = scan.next();
@@ -23,6 +23,30 @@ public class PersonInfo {
         System.out.println("ENTER PERSON EMAIL ID");
         p.emailId = scan.next();
         person.add(p);
+    }
+
+    public void editInformation(String n) {
+        for (int i = 0; i < person.size(); i++) {
+            if (n.equals(person.get(i).firstName)) {
+                person.remove(i);
+                PersonInfo p = new PersonInfo();
+                System.out.println("UPDATE PERSON FIRST NAME AND LAST NAME");
+                p.firstName = scan.next();
+                p.lastName = scan.next();
+                System.out.println("UPDATE ADDRESS AND CITY NAME");
+                p.address = scan.next();
+                p.city = scan.next();
+                System.out.println("UPDATE STATE AND ZIP");
+                p.state = scan.next();
+                p.zip = scan.next();
+                System.out.println("UPDATE PERSON PHONE NUMBER");
+                p.phoneNumber = scan.next();
+                System.out.println("UPDATE PERSON EMAIL ID");
+                p.emailId = scan.next();
+                person.add(p);
+                break;
+            }
+        }
     }
 
     public void display() {
